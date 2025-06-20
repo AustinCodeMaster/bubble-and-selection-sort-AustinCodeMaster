37c85af6aaa23dae7d42492feb15ed03decda283
AustinCodeMaster <austinndiangui@gmail.com>
2025-06-20 09:21:05 +0300

    Code showing how selection sort works

--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -1,3 +1,23 @@
-def selection_sort(arr):
-    # TODO: Implement selection sort
-    pass
+def selection_sort(nums):
+
+    for i in range(5):
+        minpos= i
+        for j in range(i,6):
+            if nums[j] <nums[minpos]:
+                minpos = j
+        
+        temp =nums[i]
+        nums[i] = nums[minpos]
+        nums[minpos]= temp
+
+nums =[5,3,8,6,7,2]
+selection_sort(nums)
+print(nums)
+# Output: [2, 3, 5, 6, 7,8]
+# The selection sort algorithm sorts the list in ascending order.
+
+            
+       
+
+
+
